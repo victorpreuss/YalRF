b4e2b1b2468b2acd905a4bab492df780910ffd55
Victor Preuss <victor.preuss@gmail.com>
2020-09-05 17:23:41 -0300

    Created a simple a methodology to test circuits
    The testing is done in Juptyer Notebooks.
    They compare YalRF to Xyce results

---
+++ tests/tb_clipper.py
@@ -0,0 +1,74 @@
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+
+import setup
+from Xyce import getXyceData
+from yarf import Yarf
+
+y = Yarf("Circuit 3")
+
+y.add_resistor('R1', 'n2', 'n3', 1e3)
+y.add_resistor('R2', 'n1', 'n2', 3.3e3)
+y.add_resistor('R3', 'n2', 'gnd', 3.3e3)
+y.add_resistor('R4', 'n4', 'gnd', 5.6e3)
+y.add_capacitor('C1', 'n2', 'n4', 0.47e-6)
+
+y.add_vdc('Vcc', 'n1', 'gnd', 5)
+vin = y.add_vdc('Vin', 'n3', 'gnd', 0)
+
+d1 = y.add_diode('D1', 'n2', 'n1')
+d2 = y.add_diode('D2', 'gnd', 'n2')
+
+d1.options['Is'] = 4e-10
+d1.options['Rs'] = 0 # not implemented yet
+d1.options['N'] = 1.48
+d1.options['Tt'] = 8e-7
+d1.options['Cj0'] = 1.95e-11
+d1.options['Vj'] = 0.4
+d1.options['M'] = 0.38
+d1.options['Eg'] = 1.36
+d1.options['Xti'] = -8
+d1.options['Kf'] = 0
+d1.options['Af'] = 1
+d1.options['Fc'] = 0.9
+d1.options['Bv'] = 600
+d1.options['Ibv'] = 1e-4
+d2.options = d1.options.copy()
+
+dc1 = y.add_dc_analysis('DC1')
+#dc1.options['reltol'] = 1e-6
+
+# get output from Xyce simulator
+xyce = getXyceData('tests/data/circuit3.prn')
+
+# sweep of Vin DC voltage
+vsweep = np.arange(-10, 15, 1)
+v2_yarf = np.empty(len(vsweep))
+v2_xyce = np.empty(len(vsweep))
+i = 0
+for v in vsweep:
+    # run dc analysis with new dc voltage
+    vin.dc = float(v)
+    x = y.run('DC1')
+
+    # get output data
+    vn1 = y.get_voltage('DC1', 'n1')
+    vn2 = y.get_voltage('DC1', 'n2')
+    vn3 = y.get_voltage('DC1', 'n3')
+    vn4 = y.get_voltage('DC1', 'n4')
+
+    # compare outputs from Xyce and Yarf
+    v2_yarf[i] = vn2
+    v2_xyce[i] = xyce[1][i][2]
+    i = i + 1
+
+print(v2_yarf)
+plt.plot(vsweep, v2_yarf)
+plt.plot(vsweep, v2_xyce)
+plt.title('Diode Clipper')
+plt.grid()
+plt.legend(['Yarf','Xyce'])
+plt.xlabel('Vsweep')
+plt.ylabel('Vclipped')
+plt.show()
--- tests/tb_diode.py
+++ tests/tb_diode.py
@@ -6,30 +6,27 @@ from yarf import Yarf
 
 y = Yarf('Diode Testbench')
 
-i1 = y.add_idc('I1', 'n1', 'gnd', 1e-3)
-y.add_resistor('R1', 'n1', 'n2', 50)
+i1 = y.add_vdc('I1', 'n1', 'gnd', 0.3)
+#y.add_resistor('R1', 'n1', 'n2', 1e3)
 
-d1 = y.add_diode('D1', 'n2', 'gnd')
+d1 = y.add_diode('D1', 'n1', 'gnd')
 d1.options['Is'] = 1e-15
 d1.options['N']  = 1
 
 dc1 = y.add_dc_analysis('DC1')
-dc1.options['max_iterations'] = 50
-
-y.run('DC1')
-
-y.print_dc_voltages('DC1')
-y.print_dc_currents('DC1')
+#dc1.options['reltol'] = 1e-9
+dc1.options['iabstol'] = 1e-12
 
 vd  = []
-idc = np.linspace(1e-3, 100e-3, 100)
+idc = np.linspace(-10, 1, 100)
 for i in idc:
     i1.dc = i
     x = y.run('DC1')
-    vd.append(x[1,0])
+    vd.append(-x[1,0])
 
 plt.figure(1)
-plt.plot(vd, idc)
+#plt.plot(vd, idc)
+plt.plot(idc, vd)
 plt.grid()
 plt.title('DC IxV curve of a Diode')
 plt.xlabel('Vd [V]')
