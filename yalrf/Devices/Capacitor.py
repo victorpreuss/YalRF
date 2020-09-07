645af38acf99c14dca29c0d063e37b58b4610df6
Victor Preuss <victor.preuss@gmail.com>
2020-09-07 15:56:23 -0300

    Added support to transient simulation in the capacitor model
    Included implicit euler and trapezoidal integration methods. Added a method to calculate and save the capacitor current, since it is not calculated by the MNA formulation but is used by some integration methods

--- yalrf/Devices/Capacitor.py
+++ yalrf/Devices/Capacitor.py
@@ -8,6 +8,11 @@ class Capacitor():
         self.n2   = n2
         self.C    = float(value)
 
+        self.I = [] # list of accepted computed currents during transient
+
+    def get_I(self):
+        return np.array(self.I)
+
     def get_num_vsources(self, analysis):
         return 0
 
@@ -15,7 +20,8 @@ class Capacitor():
         return False
 
     def init(self):
-        pass
+        # initial current at capacitor is zero
+        self.I = [0.]
 
     def add_dc_stamps(self, A, z, x, iidx):
         pass
@@ -27,7 +33,43 @@ class Capacitor():
         A[self.n1][self.n2] = A[self.n1][self.n2] - y
         A[self.n2][self.n1] = A[self.n2][self.n1] - y
 
-    # def add_tran_stamps(self, A, z, x, ):
+    def add_tran_stamps(self, A, z, x, iidx, t, tstep):
+        # get capacitor voltage and current
+        Vn = self.get_voltage(x[-1])
+        In = self.I[-1]
+
+        # calculate companion model parameters
+
+        # trapezoidal
+        geq = 2. * self.C / tstep
+        Ieq = - In - geq * Vn
+
+        # implicit euler
+        # self.geq = self.C / tstep
+        # self.Ieq = - self.geq * Vn
+
+        # add to MNA
+        A[self.n1][self.n1] = A[self.n1][self.n1] + geq
+        A[self.n2][self.n2] = A[self.n2][self.n2] + geq
+        A[self.n1][self.n2] = A[self.n1][self.n2] - geq
+        A[self.n2][self.n1] = A[self.n2][self.n1] - geq
+        z[self.n1] = z[self.n1] - Ieq
+        z[self.n2] = z[self.n2] + Ieq
+
+    def store_current(self, x, tstep):
+        # get last capacitor voltages
+        Vnprev = self.get_voltage(x[-2])
+        Vn = self.get_voltage(x[-1])
+
+        # calculate capacitor current for storage
+        geq = (2. * self.C / tstep)
+        In = geq * (Vn - Vnprev) - self.I[-1]
+        self.I.append(In)
+
+    def get_voltage(self, x):
+        V1 = x[self.n1-1] if self.n1 > 0 else 0.
+        V2 = x[self.n2-1] if self.n2 > 0 else 0.
+        return (V1 - V2)
 
     def __str__(self):
         return 'Capacitor: {}\nNodes = {} -> {}\nValue = {}\n'.format(self.name, self.n1, self.n2, self.C)
