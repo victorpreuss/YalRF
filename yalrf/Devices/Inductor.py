4ef3c1deaf6516994ebb399c8f890fb3b9d299bd
Victor Preuss <victor.preuss@gmail.com>
2020-09-07 15:49:55 -0300

    Added inductor support to transient simulation
    Included transient stamps for implicit euler and trapezoidal integration methods
    Also modified the ac stamps of the inductor so that its current is measured, like in dc and tran. This allows me to simplify get_num_vsources() of all devices, since it is not analysis-specific anymore

--- yalrf/Devices/Inductor.py
+++ yalrf/Devices/Inductor.py
@@ -9,10 +9,7 @@ class Inductor():
         self.L    = float(value)
 
     def get_num_vsources(self, analysis):
-        if analysis == 'dc':
-            return 1
-        elif analysis == 'ac':
-            return 0
+        return 1
 
     def is_nonlinear(self):
         return False
@@ -21,18 +18,44 @@ class Inductor():
         pass
 
     def add_dc_stamps(self, A, z, x, iidx):
-        A[self.n1][iidx] = +1.0 
-        A[self.n2][iidx] = -1.0 
-        A[iidx][self.n1] = +1.0 
+        A[self.n1][iidx] = +1.0
+        A[self.n2][iidx] = -1.0
+        A[iidx][self.n1] = +1.0
         A[iidx][self.n2] = -1.0
         z[iidx] = 0.0
 
     def add_ac_stamps(self, A, z, x, iidx, freq):
         y = 1.0 / (1j * 2 * np.pi * freq * self.L)
-        A[self.n1][self.n1] = A[self.n1][self.n1] + y
-        A[self.n2][self.n2] = A[self.n2][self.n2] + y
-        A[self.n1][self.n2] = A[self.n1][self.n2] - y
-        A[self.n2][self.n1] = A[self.n2][self.n1] - y
+        A[self.n1][iidx] = +1.0
+        A[self.n2][iidx] = -1.0
+        A[iidx][self.n1] = +y
+        A[iidx][self.n2] = -y
+        A[iidx][iidx] = -1.0
+
+    def add_tran_stamps(self, A, z, x, iidx, t, tstep):
+        # get inductor voltage and current
+        V1 = x[-1][self.n1-1] if self.n1 > 0 else 0.
+        V2 = x[-1][self.n2-1] if self.n2 > 0 else 0.
+        Vn = V1 - V2
+        In = x[-1][iidx-1]
+
+        # calculate companion model parameters
+
+        # trapezoidal
+        geq = tstep / (2. * self.L)
+        Ieq = In + geq * Vn
+
+        # implicit euler
+        # geq = tstep / self.L
+        # Ieq = In
+
+        # add to MNA
+        A[self.n1][iidx] = +1.0
+        A[self.n2][iidx] = -1.0
+        A[iidx][self.n1] = +geq
+        A[iidx][self.n2] = -geq
+        A[iidx][iidx] = -1.0
+        z[iidx] = z[iidx] - Ieq
 
     def __str__(self):
         return 'Inductor: {}\nNodes = {} -> {}\nValue = {}\n'.format(self.name, self.n1, self.n2, self.L)
