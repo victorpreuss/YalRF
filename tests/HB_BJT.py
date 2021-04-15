c4b82047dd9c1fbb34efb7f071d51c415c878ed3
Victor Preuss <victor.preuss@gmail.com>
2021-04-15 08:51:58 -0300

    updated testbench for new algorithm

--- tests/HB_BJT.py
+++ tests/HB_BJT.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 
 import setup
 from yalrf import YalRF, Netlist
-from yalrf.Analyses import HarmonicBalance
+from yalrf.Analyses import MultiToneHarmonicBalance
 
 y = YalRF('Diode Testbench')
 
-i1 = y.add_iac('I1', 'n1', 'gnd', ac=10e-3, phase=+90)
+f = 10e6
+
+i1 = y.add_iac('I1', 'n1', 'gnd', ac=10e-3, freq=f)
 i2 = y.add_idc('I2', 'n1', 'gnd', dc=10e-3)
 
 i3 = y.add_idc('I3', 'n2', 'gnd', dc=20e-3)
@@ -26,19 +28,37 @@ q1.options['Is'] = 1e-15
 q1.options['Bf'] = 100
 q1.options['Br'] = 1
 
-hb = HarmonicBalance('HB1', 1e6, 5)
+# q1.options['Is'] = 1.4e-14
+# q1.options['Nf'] = 1
+# q1.options['Nr'] = 1
+# q1.options['Ikf'] = 0.025
+# q1.options['Ikr'] = 1e9
+# q1.options['Vaf'] = 100
+# q1.options['Var'] = 1e12
+# q1.options['Ise'] = 3e-13
+# q1.options['Ne'] = 1.5
+# q1.options['Isc'] = 0
+# q1.options['Nc'] = 2
+# q1.options['Bf'] = 300
+# q1.options['Br'] = 7.5
 
-freqs, Vf, Vt = hb.run(y)
+# q1.options['Cje'] = 4.5e-12
+# q1.options['Vje'] = 0.75
+# q1.options['Mje'] = 0.33
+# q1.options['Cjc'] = 3.5e-12
+# q1.options['Vjc'] = 0.75
+# q1.options['Mjc'] = 0.33
+# q1.options['Xcjc'] = 1
+# q1.options['Cjs'] = 0
+# q1.options['Vjs'] = 0.75
+# q1.options['Mjs'] = 0
+# q1.options['Fc'] = 0.5
+# q1.options['Tf'] = 4e-10
+# q1.options['Tr'] = 2.1e-8
 
-n = 3
-plt.figure()
-plt.subplot(211)
-plt.plot(Vt[n,:])
-plt.grid()
-plt.subplot(212)
-plt.stem(freqs, abs(Vf[n,:]), use_line_collection=True, markerfmt='^')
-for f, v in zip(freqs, Vf[n,:]):
-    label = "({:.3f}, {:.1f})".format(abs(v), np.degrees(np.angle(v)))
-    plt.annotate(label, (f,abs(v)), textcoords="offset points", xytext=(0,10), ha='center')
-plt.grid()
+hb = MultiToneHarmonicBalance('HB1', f, 10)
+
+converged, freqs, Vf, time, Vt = hb.run(y)
+
+hb.plot_v('nc')
 plt.show()
