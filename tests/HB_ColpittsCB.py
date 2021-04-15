5c649635e4da30802a60fbd356a5c33fca3a37d3
Victor Preuss <victor.preuss@gmail.com>
2021-04-15 08:51:28 -0300

    removed base resistor and added capacitances to the transistor

--- tests/HB_ColpittsCB.py
+++ tests/HB_ColpittsCB.py
@@ -12,11 +12,11 @@ y = Netlist('Oscillator')
 # circuit parameters
 vcc = 3
 vee = -3
-rb = 100
-re = 2.2e3
+rb = 10
+re = 5e3
 l = 5e-6
 c1 = 200e-12
-c2 = 200e-12
+c2 = 50e-12
 freq = 0
 
 # VCC
@@ -33,25 +33,46 @@ y.add_gyrator('G3', 'nx3', 'nz', 'gnd', 'gnd', 1)
 
 # ideal harmonic filter of oscprobe
 Zoscprobe = y.add_idealharmonicfilter('X1', 'nz', 'nc', freq)
-Zoscprobe.g = 1e6
+Zoscprobe.g = 1e9
 
 # passives
-y.add_resistor('Rb', 'nb', 'gnd', rb)
+#y.add_resistor('Rb', 'nb', 'gnd', rb)
 y.add_resistor('Re', 'ne', 'nvee', re)
-y.add_resistor('Rx', 'nvcc', 'nc', 2e3)
-y.add_inductor('L1', 'nvcc', 'nc', l)
-y.add_capacitor('C1', 'nvcc', 'ne', c1)
-y.add_capacitor('C2', 'ne', 'nc', c2)
+y.add_resistor('Rx', 'nvccx', 'nvcc', 0.5)
+y.add_inductor('L1', 'nvccx', 'nc', l)
+y.add_capacitor('C1', 'ne', 'nc', c1)
+y.add_capacitor('C2', 'nvcc', 'ne', c2)
 
 # bjts
-q1 = y.add_bjt('Q1', 'nb', 'nc', 'ne')
+q1 = y.add_bjt('Q1', 'gnd', 'nc', 'ne')
 
-q1.options['Is'] = 1e-16
-q1.options['Bf'] = 100
-q1.options['Br'] = 1
+q1.options['Is'] = 10.2e-15
+q1.options['Bf'] = 301
+q1.options['Br'] = 4
+q1.options['Ne'] = 2
+q1.options['Nc'] = 2
+q1.options['Ise'] = 5.82e-12
+q1.options['Vaf'] = 121
+q1.options['Var'] = 24
+q1.options['Ikf'] = 60.7e-3
+q1.options['Ikr'] = 0.15
+
+q1.options['Cje'] = 26.8e-12
+q1.options['Vje'] = 1.1
+q1.options['Mje'] = 0.5
+q1.options['Cjc'] = 8.67e-12
+q1.options['Vjc'] = 0.3
+q1.options['Mjc'] = 0.3
+q1.options['Xcjc'] = 1
+q1.options['Cjs'] = 0
+q1.options['Vjs'] = 0.75
+q1.options['Mjs'] = 0
+q1.options['Fc'] = 0.5
+q1.options['Tf'] = 427e-12
+q1.options['Tr'] = 50.3e-9
 
 hb = MultiToneHarmonicBalance('HB1', 1, 10)
-hb.options['maxiter'] = 100
+hb.options['maxiter'] = 150
 Vprev = 0
 
 def objFunc(x, info):
@@ -79,7 +100,7 @@ def objFunc(x, info):
 
     # if HB failed to converge, return a bad convergence value to minimizer
     if not converged:
-        return 1e6
+        return 1e2
 
     # get nodes of IdealHarmonicFilter
     n1 = hb.get_node_idx('nc')
@@ -96,16 +117,19 @@ def objFunc(x, info):
 
     return abs(Yosc)
 
-x0 = [ 5e6, .5]
+b  = [(5e6, 15e6), (1, 4)]
+x0 = [ 10e6, 1]
 result = optimize.fmin(func     = objFunc,
                        x0       = x0,
                        args     = ({'itercnt' : 0},),
-                       xtol     = 1e-5,
-                       ftol     = 1e-5,
-                       maxfun   = 150,
+                       xtol     = 1e-3,
+                       ftol     = 1e-3,
+                       maxfun   = 200,
                        disp     = True,
                        retall   = False,
                        full_output = True)
+
+#result = optimize.minimize(fun=objFunc, x0=x0, method='Powell', bounds=b, args=({'itercnt' : 0},))
 
 xopt = result[0]
 
