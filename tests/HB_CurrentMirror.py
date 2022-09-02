f31ce655cabdbcdf444359add96752240a694d7f
Victor Preuss <victor.preuss@gmail.com>
2022-09-02 09:06:47 -0300

    Updated this testbench for MTHB engine

--- tests/HB_CurrentMirror.py
+++ tests/HB_CurrentMirror.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 import setup
 from yalrf import YalRF, Netlist
-from yalrf.Analyses import HarmonicBalance
+from yalrf.Analyses import MultiToneHarmonicBalance
 
 y = YalRF('Diode Testbench')
 
-i1 = y.add_iac('I1', 'n1', 'gnd', ac=3e-3, phase=+90)
+i1 = y.add_iac('I1', 'n1', 'gnd', ac=3e-3, freq=1e6, phase=+90)
 i2 = y.add_idc('I2', 'n1', 'gnd', dc=20e-3)
 
 r1 = y.add_resistor('R1', 'n1', 'gnd', 200)
@@ -24,19 +24,22 @@ q1.options['Is'] = 1e-15
 q1.options['Bf'] = 100
 q1.options['Vaf'] = 20
 
-hb = HarmonicBalance('HB1', 1e6, 5)
+hb = MultiToneHarmonicBalance('HB1', 1e6, 5)
 
-freqs, Vf, Vt = hb.run(y)
+converged, freqs, Vf, time, Vt = hb.run(y)
 
-n = 1
-plt.figure()
-plt.subplot(211)
-plt.plot(Vt[n,:])
-plt.grid()
-plt.subplot(212)
-plt.stem(freqs, abs(Vf[n,:]), use_line_collection=True, markerfmt='^')
-for f, v in zip(freqs, Vf[n,:]):
-    label = "({:.3f}, {:.1f})".format(abs(v), np.degrees(np.angle(v)))
-    plt.annotate(label, (f,abs(v)), textcoords="offset points", xytext=(0,10), ha='center')
-plt.grid()
+hb.plot_v('nc')
 plt.show()
+
+# n = 1
+# plt.figure()
+# plt.subplot(211)
+# plt.plot(Vt[n,:])
+# plt.grid()
+# plt.subplot(212)
+# plt.stem(freqs, abs(Vf[n,:]), use_line_collection=True, markerfmt='^')
+# for f, v in zip(freqs, Vf[n,:]):
+#     label = "({:.3f}, {:.1f})".format(abs(v), np.degrees(np.angle(v)))
+#     plt.annotate(label, (f,abs(v)), textcoords="offset points", xytext=(0,10), ha='center')
+# plt.grid()
+# plt.show()
